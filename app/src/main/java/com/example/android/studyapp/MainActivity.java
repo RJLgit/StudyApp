555563fbc49981209fd74af8656d93da3b0e20d4
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-20 08:11:29 +0100

    Navigates through fragments based on the navigation item clicked

--- app/src/main/java/com/example/android/studyapp/MainActivity.java
+++ app/src/main/java/com/example/android/studyapp/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.android.studyapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +29,39 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setTitle(getString(R.string.app_name));
         toolbar.setSubtitle("Manage your study tasks");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.replace(R.id.fragment_container, new TasksFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d(TAG, "onNavigationItemSelected: ");
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        switch (id) {
+            case R.id.menu_todo:
+                transaction.replace(R.id.fragment_container, new TasksFragment()).commit();
+                break;
+            case R.id.menu_done:
+                transaction.replace(R.id.fragment_container, new CompletedFragment()).commit();
+                break;
+            case R.id.menu_postponed:
+                transaction.replace(R.id.fragment_container, new PostponedFragment()).commit();
+                break;
+            default:
+                transaction.replace(R.id.fragment_container, new TasksFragment()).commit();
+                break;
+        }
+
+        return true;
     }
 }
