a1038108581067755a7be1d01bf62fffd1aeebf7
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-20 07:48:11 +0100

    Assigned toolbar in java code

--- app/src/main/java/com/example/android/studyapp/MainActivity.java
+++ app/src/main/java/com/example/android/studyapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.android.studyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        toolbar.setSubtitle("Manage your study tasks");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
