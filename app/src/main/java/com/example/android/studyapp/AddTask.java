aa8eb758a2c5c806b347f5c516ffaf2f96f75dff
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-22 08:25:10 +0100

    Added categories to the spinner

--- app/src/main/java/com/example/android/studyapp/AddTask.java
+++ app/src/main/java/com/example/android/studyapp/AddTask.java
@@ -6,8 +6,10 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
@@ -17,6 +19,7 @@ public class AddTask extends AppCompatActivity {
     Button addButton;
     NumberPicker numberPicker;
     Spinner spinner;
+    private static final String TAG = "AddTask";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +35,20 @@ public class AddTask extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "onClick: " + spinner.getSelectedItem().toString());
                 finish();
             }
         });
 
         numberPicker = findViewById(R.id.priorityNumberPicker);
-        spinner = findViewById(R.id.categorySpinner);
         numberPicker.setMinValue(1);
         numberPicker.setMaxValue(5);
+
+        spinner = findViewById(R.id.categorySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
     }
 
