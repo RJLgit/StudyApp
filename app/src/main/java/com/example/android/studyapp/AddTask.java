d827d7f3fa589686e1bb2c31e7a81810d9298a3a
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-22 08:19:13 +0100

    Put priority and category on the same line and set min max

--- app/src/main/java/com/example/android/studyapp/AddTask.java
+++ app/src/main/java/com/example/android/studyapp/AddTask.java
@@ -9,10 +9,15 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
 
 public class AddTask extends AppCompatActivity {
     Toolbar toolbar;
     Button addButton;
+    NumberPicker numberPicker;
+    Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +35,11 @@ public class AddTask extends AppCompatActivity {
                 finish();
             }
         });
+
+        numberPicker = findViewById(R.id.priorityNumberPicker);
+        spinner = findViewById(R.id.categorySpinner);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(5);
 
     }
 
