b40ed28a074a3e5184f32239374b141ba005b641
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-22 08:14:07 +0100

    Added onclick listener to addtask button

--- app/src/main/java/com/example/android/studyapp/AddTask.java
+++ app/src/main/java/com/example/android/studyapp/AddTask.java
@@ -7,9 +7,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class AddTask extends AppCompatActivity {
     Toolbar toolbar;
+    Button addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class AddTask extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setSubtitle("Add a task");
+
+        addButton = findViewById(R.id.addTaskButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 
