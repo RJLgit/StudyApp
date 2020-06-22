b66b4b0994c92a95e2fcfc9bc7c6d08f03a0bf72
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-22 05:13:19 +0100

    Added toolbar to add task activity

--- app/src/main/java/com/example/android/studyapp/AddTask.java
+++ app/src/main/java/com/example/android/studyapp/AddTask.java
@@ -1,14 +1,21 @@
 package com.example.android.studyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class AddTask extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setSubtitle("Add a task");
+
     }
 }
