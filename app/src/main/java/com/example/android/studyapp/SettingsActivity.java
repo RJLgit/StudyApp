9852a3f8b6623c31a354a2607b5e7571981f3a77
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-24 05:50:58 +0100

    Added settings activity and layout

---
+++ app/src/main/java/com/example/android/studyapp/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.studyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        
+    }
+}
