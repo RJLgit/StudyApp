b52f805404c8fd620cd28425f7d034f43de47fdc
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-24 04:47:07 +0100

    Added two other recycler views

--- app/src/main/java/com/example/android/studyapp/CompletedFragment.java
+++ app/src/main/java/com/example/android/studyapp/CompletedFragment.java
@@ -5,7 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.studyapp.data.Task;
+import com.example.android.studyapp.data.TaskViewModel;
+
+import java.util.List;
+
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class CompletedFragment extends Fragment {
@@ -21,7 +29,19 @@ public class CompletedFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_completed, container, false);
 
+        recyclerView = v.findViewById(R.id.completed_rv);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
 
+        final TaskAdapter adapter = new TaskAdapter(getActivity());
+
+        TaskViewModel viewModel = new ViewModelProvider(requireActivity()).get(TaskViewModel.class);
+        viewModel.getCompletedTasks().observe(getViewLifecycleOwner(), new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                adapter.setMyTasks(tasks);
+            }
+        });
 
         return v;
     }
--- app/src/main/java/com/example/android/studyapp/PostponedFragment.java
+++ app/src/main/java/com/example/android/studyapp/PostponedFragment.java
@@ -5,10 +5,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.studyapp.data.Task;
+import com.example.android.studyapp.data.TaskViewModel;
+
+import java.util.List;
+
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PostponedFragment extends Fragment {
 
+    RecyclerView recyclerView;
 
     public PostponedFragment() {
         // Required empty public constructor
@@ -18,6 +28,22 @@ public class PostponedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_postponed, container, false);
+        View v = inflater.inflate(R.layout.fragment_postponed, container, false);
+
+        recyclerView = v.findViewById(R.id.postponed_rv);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        final TaskAdapter adapter = new TaskAdapter(getActivity());
+
+        TaskViewModel viewModel = new ViewModelProvider(requireActivity()).get(TaskViewModel.class);
+        viewModel.getPostponedTasks().observe(getViewLifecycleOwner(), new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                adapter.setMyTasks(tasks);
+            }
+        });
+
+        return v;
     }
 }
