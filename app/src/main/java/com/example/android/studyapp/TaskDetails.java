9c1ddbd9a86ebb18fdb61c99775c71bf9f0076f0
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-25 05:16:31 +0100

    Assigned UI elements in details activity

--- app/src/main/java/com/example/android/studyapp/TaskDetails.java
+++ app/src/main/java/com/example/android/studyapp/TaskDetails.java
@@ -1,14 +1,36 @@
 package com.example.android.studyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TaskDetails extends AppCompatActivity {
+    Toolbar toolbar;
+    TextView title, date, priority, category, description, status;
+    Button editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_details);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        title = findViewById(R.id.titleTextView);
+        date = findViewById(R.id.dateTextView);
+        priority = findViewById(R.id.priorityTextView);
+        category = findViewById(R.id.categoryTextView);
+        description = findViewById(R.id.descriptionTextView);
+        status = findViewById(R.id.currentStatusTextView);
+        editButton = findViewById(R.id.editTaskButton);
+
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getString(R.string.app_name));
+        toolbar.setSubtitle("Set title of task here");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
