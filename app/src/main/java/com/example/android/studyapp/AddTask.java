3cc08253556801f896b39a74ee7788e335de131f
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-29 08:31:15 +0100

    Tidied up add task class

--- app/src/main/java/com/example/android/studyapp/AddTask.java
+++ app/src/main/java/com/example/android/studyapp/AddTask.java
@@ -21,34 +21,32 @@ import com.example.android.studyapp.data.Task;
 import com.example.android.studyapp.data.TaskViewModel;
 
 public class AddTask extends AppCompatActivity {
+    //UI elements
     Toolbar toolbar;
     Button addButton;
     NumberPicker numberPicker;
     Spinner spinner;
     EditText titleEditText;
     EditText descriptionEditText;
+
     private static final String TAG = "AddTask";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
-
+        //Sets up the toolbar
         toolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setSubtitle("Add a task");
-
+        toolbar.setSubtitle(R.string.add_task_toolbar_subtitle);
+        //Gets the Viewmodel instance
         final TaskViewModel viewModel = new ViewModelProvider(this).get(TaskViewModel.class);
-
-
-
-
+        //Sets the number picker to take values between 1 and 5
         numberPicker = findViewById(R.id.priorityNumberPicker);
         numberPicker.setMinValue(1);
         numberPicker.setMaxValue(5);
-
-
+        //Sets up the spinner with the categories array of values
         spinner = findViewById(R.id.categorySpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.categories, android.R.layout.simple_spinner_item);
@@ -57,13 +55,14 @@ public class AddTask extends AppCompatActivity {
 
         titleEditText = findViewById(R.id.edit_text_title);
         descriptionEditText = findViewById(R.id.edit_text_description);
-
         addButton = findViewById(R.id.addTaskButton);
+        //When the add button is pressed the entries are checked for validity
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: " + spinner.getSelectedItem().toString());
                 if (checkValidEntry()) {
+                    //If the entry is valid then a new task object is created from the data entered
                     String title = titleEditText.getText().toString();
                     String description = descriptionEditText.getText().toString();
                     int priority = numberPicker.getValue();
@@ -72,18 +71,22 @@ public class AddTask extends AppCompatActivity {
                     boolean postponed = false;
                     long time = System.currentTimeMillis();
                     Task task = new Task(title, description, priority, category, completed, time, postponed);
+                    //This task object is inserted into the database.
                     viewModel.insertTask(task);
+                    //The edit texts are cleared for the next entry
                     titleEditText.getText().clear();
                     descriptionEditText.getText().clear();
                 } else {
-                    Toast.makeText(AddTask.this, "Please enter text for both the title and description", Toast.LENGTH_LONG).show();
+                    //A toast is shown if the entry is not valid
+                    Toast.makeText(AddTask.this, R.string.add_task_toast_not_valid, Toast.LENGTH_LONG).show();
                 }
 
             }
         });
 
     }
-
+    //This helper method checks whether the user has entered a title and description
+    //If they have then true is returned, otherwise it returns false.
     private boolean checkValidEntry() {
         if (titleEditText.getText().toString().trim().equals("") ||
                 descriptionEditText.getText().toString().trim().equals("")) {
@@ -92,11 +95,10 @@ public class AddTask extends AppCompatActivity {
             return true;
         }
     }
-
+    //Implements the back button on the toolbar functionality.
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
-
         switch(itemId) {
             case android.R.id.home:
                 onBackPressed();
@@ -104,10 +106,9 @@ public class AddTask extends AppCompatActivity {
             default:
                 break;
         }
-
         return super.onOptionsItemSelected(item);
     }
-
+    //Implements on back pressed functionality to use the animations
     @Override
     public void onBackPressed() {
         super.onBackPressed();
